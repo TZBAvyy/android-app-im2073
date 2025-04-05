2ee8e9163889c414488eabea43b9446833b52e05
Avi <gibraltar.av@gmail.com>
2025-04-05 17:29:47 +0800

    Add Http functionality to app
    - Btn uses btnOnClickHandler to create HttpTask (AsyncTask) that sends GET request to Tomcat Server
    - Create basic structure to clicker app

--- app/src/main/java/com/example/android_app_im2073/MainActivity.java
+++ app/src/main/java/com/example/android_app_im2073/MainActivity.java
@@ -7,9 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void btnOnClickHandler(View v) {
+        String btnChoice = v.getTag().toString();
+        new HttpTask().execute("http://10.0.2.2:9999/quiz/select?question_id=1&player_id=1&choice="+btnChoice);  // Tomcat at localhost:9999
+        Toast.makeText(this, "Send", Toast.LENGTH_LONG).show(); // Toast a message
+    }
+
+    // Run the HTTP request in a background thread, separating from the main UI thread
+    private class HttpTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strURLs) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            try {
+                url = new URL(strURLs[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                // Get the HTTP response code (e.g., 200 for "OK", 404 for "Not found")
+                // and pass a string description in result to onPostExecute(String result)
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {  // 200
+                    return "OK (" + responseCode + ")";
+                } else {
+                    return "Fail (" + responseCode + ")";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
     }
 }
